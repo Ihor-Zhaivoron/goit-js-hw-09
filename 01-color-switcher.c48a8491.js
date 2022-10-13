8082cfa2c620205c29d1df7236a272ef33c710fb
Ihor-Zhaivoron <105306196+Ihor-Zhaivoron@users.noreply.github.com>
2022-10-13 11:20:09 +0000

    Deploying to gh-pages from @ Ihor-Zhaivoron/goit-js-hw-09@a6fda2bbb65e7d4cb2d2039e67a0526f0401ff66 🚀

---
+++ 01-color-switcher.c48a8491.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("body"),t=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]");t.addEventListener("click",(function(){t.disabled=!0,n.disabled=!1,a=setInterval((function(){return e.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),n.addEventListener("click",(function(){clearInterval(a),n.disabled=!0,t.disabled=!1}));var a=null}();
+//# sourceMappingURL=01-color-switcher.c48a8491.js.map
