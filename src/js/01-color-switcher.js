a6fda2bbb65e7d4cb2d2039e67a0526f0401ff66
Ihor-Zhaivoron <105306196+Ihor-Zhaivoron@users.noreply.github.com>
2022-10-13 14:19:33 +0300

    1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const body = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+startBtn.addEventListener('click', onStartBtn);
+stopBtn.addEventListener('click', onStopBtn);
+
+let intervald = null;
+
+function onStartBtn() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  intervald = setInterval(
+    () =>
+      (body.style.backgroundColor = `#${Math.floor(
+        Math.random() * 16777215
+      ).toString(16)}`),
+    1000
+  );
+}
+function onStopBtn() {
+  clearInterval(intervald);
+  stopBtn.disabled = true;
+  startBtn.disabled = false;
+}
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
