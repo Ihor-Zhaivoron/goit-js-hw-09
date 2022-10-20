5ff75120b05dc6950ea69312ef11bb4ebf599c72
Ihor-Zhaivoron <105306196+Ihor-Zhaivoron@users.noreply.github.com>
2022-10-20 13:18:49 +0300

    3 task error

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const delayEl = document.querySelector('input[name="delay"]');
+const stepEl = document.querySelector('input[name="step"]');
+const amountEl = document.querySelector('input[name="amount"]');
+const btn = document.querySelector('button');
+
+btn.addEventListener('submit', onSubmitBtn);
+
+function onSubmitBtn(e) {
+  e.preventDefault();
+  let delay = Number(delayEl.value);
+
+  setTimeout(() => {
+    for (let i = 0; i <= amountEl.value; i++) {
+      createPromise(i, delay)
+        .then(({ position, delay }) => {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+      delay += Number(stepEl.value);
+    }
+  }, delay);
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay }); // Fulfill
+      } else {
+        reject({ position, delay }); // Reject
+      }
+    }, delay);
+  });
 }
+
+// createPromise(1, 1500)
+//   .then(({ position, delay }) => {
+//     Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
